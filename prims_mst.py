f82cb28f29d61e523dc4c2397bdc641e80b12f86
mariaKozlovtseva <maria.kozlovceva@gmail.com>
2020-10-31 14:32:21 +0300

    first commit

---
+++ prims_mst.py
@@ -0,0 +1,48 @@
+from indexed_priority_queue import IndexedPriorityQueue, transform_data, inv_transform
+from graph import Graph
+
+def prims_algo(g, start, inv_ids, ids):
+    ipq = IndexedPriorityQueue(inv_ids)
+    for vertex in g:
+        vertex.setDist(float('inf'))
+        vertex.setPred(None)
+    start.setDist(0)
+    ipq.build_heap(dict([(ids[vertex.getId()], vertex.getDist()) for vertex in g]))
+
+    while ipq.size:
+        key, val = ipq.extract_min()
+        node = g.getVertex(inv_ids[key])
+        for neighbour in node.getConnections():
+            new_val = val + node.getWeight(neighbour)
+            if ids[neighbour.getId()] in ipq and new_val < neighbour.getDist():
+                neighbour.setPred(node)
+                neighbour.setDist(new_val)
+                ipq.decrease_key(ids[neighbour.getId()], new_val)
+
+    res = []
+    for vertex in g.getVertices():
+        vertex = g.getVertex(vertex)
+        if not vertex.getPred():
+            res.append((vertex.getPred(), vertex.getId(), vertex.getDist()))
+        else:
+            res.append((vertex.getPred().getId(), vertex.getId(), vertex.getDist()))
+    return res
+
+
+
+if __name__ == '__main__':
+    keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    ids = transform_data(keys)
+    inverse_ids = inv_transform(keys)
+    d = {
+        'A': {'B': 2, 'C': 3},
+        'B': {'A': 2, 'C': 1, 'D': 1, 'E': 4},
+        'C': {'A': 3, 'B': 1, 'F': 5},
+        'D': {'B': 1, 'E': 1},
+        'E': {'B': 4, 'F': 1, 'D': 1},
+        'F': {'C': 5, 'G': 1, 'E': 1},
+        'G': {'F': 1}
+    }
+    g = Graph()
+    g.build_graph(d)
+    print(prims_algo(g, g.getVertex('A'), inverse_ids, ids))
