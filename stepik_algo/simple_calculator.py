980871e1725c726175f57393205a6737c67153a2
mariaKozlovtseva <maria.kozlovceva@gmail.com>
2021-01-25 14:03:47 +0300

    add description to algorithm

--- stepik_algo/simple_calculator.py
+++ stepik_algo/simple_calculator.py
@@ -1,4 +1,12 @@
 def simple_calculator(n):
+    """
+    You have a primitive calculator that can perform only three operations
+    on the current number x: replace x with 2*x, 3*x or x + 1.
+    Given integer N find the minimum number of operations K
+    required to get to N out of 1.
+    Print K and a sequence of intermediate numbers.
+
+    """
     opers = [lambda x: 3*x, lambda x: 2*x, lambda x: x+1]
     d = list(range(n))
     # will be used in the end to make a sequence of calculated values to reach n
